244ac5f78b1c740f4d1862dd7b5ccc5fa022e2c3
victoriaguille <91439219+victoriaguille@users.noreply.github.com>
2021-10-25 20:45:47 -0400

    Update PyPoll.py
    added candidate name pulling code

--- PyPoll.py
+++ PyPoll.py
@@ -13,18 +13,41 @@ file_to_load = os.path.join('resources', 'election_results.csv')
 #assign a variable to save the file to a path
 file_to_save = os.path.join("analysis", "election_analysis.txt")
 
+#Initialize a total vote counter
+total_votes = 0
+
+#candidate options
+candidate_options = []
+
 #open the election results and read the file
 with open(file_to_load) as election_data:
     file_reader = csv.reader(election_data)
 
-    #read and print the header row
+    #read the header row
     headers = next(file_reader)
-    print(headers)
 
-    
     #print each row in the csv file
     for row in file_reader:
-        print(row)
+        #add to the total vote count
+        total_votes += 1
+
+        #print the candidate name from each row
+        candidate_name = row[2]
+
+        #if the candidate does not match any existing candidate...
+        if candidate_name not in candidate_options:
+            #add it to the list of candidates
+            candidate_options.append(candidate_name)
+
+
+#print the candidate list
+print(candidate_options)
+
+#3. print the total votes
+print(total_votes)
+
+
+
 #using the with statement open the file as a text file
 with open(file_to_save, "w") as txt_file:
 
